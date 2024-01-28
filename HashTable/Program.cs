4c534071763d33b3fbd8fd2568e236ca062accaa
ehwjr0032@naver.com <ehwjr0032@naver.com>
2024-01-28 17:39:12 +0900

    HashTable

---
+++ HashTable/Program.cs
@@ -0,0 +1,52 @@
+﻿using HashTable;
+
+namespace HashTable
+{
+    public class CheatKey
+    {
+        Action showmethemoney;
+        Action thereisnocowlevel;
+        public CheatKey()
+        {
+            showmethemoney += ShowMeTheMoney;
+            thereisnocowlevel += ThereIsNoCowLevel;
+            cheatDic = new Dictionary<string, Action>(2);
+            cheatDic.Add("SHOWMETHTMONEY", showmethemoney);
+            cheatDic.Add("THEREISNOCOWLEVEL", thereisnocowlevel);
+        }
+
+        private Dictionary<string, Action> cheatDic;
+
+        public void Run(string cheatKey)
+        {
+            cheatDic.TryGetValue(cheatKey, out Action Value);
+            Value();
+        }
+
+        public void ShowMeTheMoney()
+        {
+            Console.WriteLine("골드를 늘려주는 치트키 발동");
+        }
+
+        public void ThereIsNoCowLevel()
+        {
+            Console.WriteLine("바로 승리하는 치트키 발동");
+        }
+         
+    }
+     
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CheatKey cheatkey = new CheatKey();
+            while (true)
+            {
+                string keyinput = Console.ReadLine();
+                cheatkey.Run(keyinput);
+            }
+        }
+    }
+}
+
+
