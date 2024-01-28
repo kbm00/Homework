84f62ef670f374150cd0e5e902cc0a566e36760e
ehwjr0032@naver.com <ehwjr0032@naver.com>
2024-01-28 17:40:26 +0900

    Searching

---
+++ Searching/Graph.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searching
+{
+    internal class Graph
+    {
+        bool[,] MatrixGraph = new bool[8, 8]
+         {
+          {false, false,  true, false,  true, false, false, false},
+          {false, false,  true, false, false,  true, false, false},
+          { true,  true, false, false, false,  true,  true, false},
+          {false, false, false, false, false ,false, false, true },
+          { true, false, false, false, false, false, false, true },
+          {false,  true,  true, false, false, false, false, false},
+          {false, false,  true, false, false, false, false, false},
+          {false, false, false,  true,  true, false, false, false},
+         };
+
+        bool[,] MatrixGraph2 = new bool[8, 8]
+     {
+          {false, false, false, false, false, false, false, false},
+          {false, false, false, false, false, false, false, false},
+          {false, false, false, false,  true,  true, false, false},
+          {false,  true, false, false, false,  true, false, true },
+          {false, false, false, false, false, false, false, false},
+          {false,  true, false, false, false, false, false, false},
+          {false, false,  true, false, false,  true, false, false},
+          {false, false, false, false, false, false,  true, false},
+     };
+         
+        const int INF = int.MaxValue;
+        int[,] MatrixGraph3 = new int[8, 8]
+       {
+            {  0,   4, INF, INF,   6, INF, INF, INF},
+            {  4,   0,   5,   4, INF,   8,   2, INF},
+            {INF,   5,   0, INF,   9, INF, INF, INF},
+            {INF,   4, INF,   0, INF, INF, INF, INF},
+            {  6, INF,   9, INF,  0,  INF,   5, INF},
+            {INF,   8, INF, INF, INF,   0, INF,   1},
+            {INF,   2, INF, INF,   5, INF,   0, INF},
+            {INF, INF, INF, INF, INF,   1, INF,   0},
+       };
+
+        
+
+
+    }
+}
---
+++ Searching/Program.cs
@@ -0,0 +1,127 @@
+﻿namespace Searching
+{
+    internal class Searching
+    {
+        public static void DFS(bool[,] graph, int start, out bool[] visited, out int[] parents)
+        {
+            visited = new bool[graph.GetLength(0)];
+            parents = new int[graph.GetLength(0)];
+
+            for(int i = 0; i < graph.GetLength(0); i++)
+            {
+                visited[i] = false;
+                parents[i] = -1;
+            }
+            SearchNode(graph, start, visited, parents);
+ 
+        }
+
+        private static void SearchNode(bool[,] graph, int start, bool[] visited, int[] parents)
+        {
+            visited[start] = true;
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                if (graph[start, i] &&
+                      !visited[i])
+                {
+                    parents[i] = start;
+                    SearchNode(graph, i, visited, parents);
+                }
+            }
+        }
+
+        public static void BFS(bool[,] graph, int start, out bool[] visited, out int[] parents)
+        {
+            visited = new bool[graph.GetLength(0)];
+            parents = new int[graph.GetLength(0)];
+
+            for( int i =0; i< graph.GetLength(0); i++)
+            {
+                visited[i] = false;
+                parents[i] = -1;
+
+            }
+            visited[start] = true;
+
+            Queue<int> bfsQueue = new Queue<int>();
+
+            bfsQueue.Enqueue(start);
+            while (bfsQueue.Count > 0) 
+            {
+              int next = bfsQueue.Dequeue();
+
+                for(int i = 0; i < graph.GetLength(0); i++)
+                {
+                    if (graph[next, i] &&
+                        !visited[i])
+                    {
+                        visited[i] = true;
+                        parents[i] = next;
+                        bfsQueue.Enqueue(i);
+                    }
+
+                }
+            }
+        }
+    }   
+  
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+           /* bool[,] graph = new bool[8, 8]
+            {
+                { false,  true, false, false, false, false, false, false },
+                {  true, false,  true, false, false,  true, false, false },
+                { false,  true, false, false,  true,  true, false, false },
+                { false, false, false, false, false,  true, false, false },
+                { false, false,  true, false, false, false,  true,  true },
+                { false,  true,  true,  true, false, false, false, false },
+                { false, false, false, false,  true, false, false, false },
+                { false, false, false, false,  true, false, false, false },
+            };*/ 
+            bool[,] graph = new bool[8, 8]
+            {
+                { false, false, false, true, true, false, false, false },
+                { false, false,  false, true, false,  true, true, false },
+                { false,  false, false, false,  false,  false, true, false },
+                { true,true, false, false, false,  true, false, true },
+                { true, false,  false, false, false, false,  true,  false },
+                { false,  true,  false,  true, false, false, true, true },
+                { false, false, true, false,  true, true, false, true },
+                { false, false, false, true,  false, true,true, false },
+            };
+
+            // DFS 탐색
+            bool[] dfsVisited;
+            int[] dfsPath;
+            Searching.DFS(graph, 0, out dfsVisited, out dfsPath);
+            Console.WriteLine("<DFS>");
+            PrintGraphSearch(dfsVisited, dfsPath);
+            Console.WriteLine();
+
+            // BFS 탐색
+            bool[] bfsVisited;
+            int[] bfsPath;
+            Searching.BFS(graph, 0, out bfsVisited, out bfsPath);
+            Console.WriteLine("<BFS>");
+            PrintGraphSearch(bfsVisited, bfsPath);
+            Console.WriteLine();
+        }
+
+        private static void PrintGraphSearch(bool[] visited, int[] path)
+        {
+            Console.WriteLine($"{"Vertex",8}{"Visit",8}{"Path",8}");
+
+            for (int i = 0; i < visited.Length; i++)
+            {
+                Console.WriteLine($"{i,8}{visited[i],8}{path[i],8}");
+            }
+        }
+
+
+
+    }
+    
+}
